29c9530ffcd3ec681fcfe97cdf028ba2059e7163
Adam Kopeć <adamkopec5@icloud.com>
2022-04-22 22:30:20 +0200

    Initial Commit

---
+++ Chess Clock/Chess_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chess_ClockApp.swift
+//  Chess Clock
+//
+//  Created by Adam Kopeć on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Chess_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
